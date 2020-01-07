bf42eca86d82c7d87818e422d9fa21c6ef1d5a99
Egor <yytruten@mts.ru>
2020-01-08 01:22:10 +0300

    introduce ways to compose handlers (they seem terribly similar to Kleisli arrows

--- core/src/commonMain/kotlin/dolphins/core/Handler.kt
+++ core/src/commonMain/kotlin/dolphins/core/Handler.kt
@@ -8,6 +8,30 @@ interface Handler<F, in E, out M> : FunDeps<F> {
 
 }
 
+fun <F, E, M> handler(deps: FunDeps<F>, handle: (E) -> Kind<F, M>): Handler<F, E, M> =
+    object : Handler<F, E, M>, FunDeps<F> by deps {
+
+        override fun handle(e: E): Kind<F, M> =
+            handle(e)
+
+    }
+
+fun <F, E1, E2, M> Handler<F, E2, M>.preMap(handler: Handler<F, E1, E2>): Handler<F, E1, M> =
+    handler(this) { e1 ->
+        handler.handle(e1)
+            .flatMap { e2 ->
+                this.handle(e2)
+            }
+    }
+
+fun <F, E, M1, M2> Handler<F, E, M1>.postMap(handler: Handler<F, M1, M2>): Handler<F, E, M2> =
+    handler(this) { e ->
+        this.handle(e)
+            .flatMap { m ->
+                handler.handle(m)
+            }
+    }
+
 class IdentityCofx<F, M>(funDeps: FunDeps<F>) : Handler<F, M, M>, FunDeps<F> by funDeps {
 
     override fun handle(e: M): Kind<F, M> =
