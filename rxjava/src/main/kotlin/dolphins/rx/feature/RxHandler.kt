c8a1ef7fea08e4592d8de358aa0f4be459707fcf
Egor <yytruten@mts.ru>
2020-03-12 18:10:49 +0300

    extend rx handler

--- rxjava/src/main/kotlin/dolphins/rx/feature/RxHandler.kt
+++ rxjava/src/main/kotlin/dolphins/rx/feature/RxHandler.kt
@@ -1,8 +1,9 @@
 package dolphins.rx.feature
 
-import dolphins.core.FunDeps
-import dolphins.core.Handler
 import dolphins.foundation.Kind
+import dolphins.newcore.*
+import dolphins.rx.instances.observable.Monad
+import dolphins.rx.instances.observable.Stream
 import dolphins.rx.types.Rx
 import dolphins.rx.types.unfix
 import io.reactivex.Observable
@@ -10,18 +11,33 @@ import io.reactivex.Observable
 /**
  * Override this to create effect and coeffect handlers.
  */
-abstract class RxHandler<E, M> : Handler<Rx, E, M>, FunDeps<Rx> by RxDepBag {
+abstract class RxHandler<In, Out> : Handler<Rx, In, Out> {
 
-    override fun kindfulHandle(e: E): Kind<Rx, M> = handle(e).unfix()
+    final override fun fHandle(input: In): Kind<Rx, Out> = handle(input).unfix()
 
-    abstract fun handle(e: E): Observable<M>
+    abstract fun handle(input: In): Observable<Out>
+
+    companion object
 
 }
 
-/**
- * Creates handler out of air
- */
-fun <E, M> rxHandler(handle: (E) -> Observable<M>): RxHandler<E, M> =
-    object : RxHandler<E, M>() {
-        override fun handle(e: E): Observable<M> = handle(e)
+fun <A> RxHandler.Companion.Identity(): Handler<Rx, A, A> =
+    Handler.Identity(Rx.Monad)
+
+fun <A, B> RxHandler.Companion.FromFunction(perform: (A) -> Observable<B>): Handler<Rx, A, B> =
+    object : RxHandler<A, B>() {
+
+        override fun handle(input: A): Observable<B> {
+            return perform(input)
+        }
+
     }
+
+fun RxHandler.Companion.Idle(): Handler<Rx, Any, Nothing> =
+    Handler.Idle(Rx.Stream)
+
+infix fun <A, B, C> RxHandler<A, B>.then(handler: RxHandler<B, C>): Handler<Rx, A, C> =
+    then(Rx.Monad, handler)
+
+infix fun <A, B, C> RxHandler<B, C>.dot(handler: RxHandler<A, B>): Handler<Rx, A, C> =
+    dot(Rx.Monad, handler)
